3b214472e0b763b2a45dbabcb99328dba9b4ddf8
Pilks-pixel <peterpilkington@hotmail.com>
2025-10-14 10:06:06 +0800

    added BFS graph

---
+++ graphs/breadth-first-search.js
@@ -0,0 +1,10 @@
+// breadth-first search (BFS) implementation
+// find the shortest path
+const graph = {
+    A: ['B', 'C'],
+    B: ['D', 'E'],
+    C: ['F'],
+    D: [],
+    E: ['F'],
+    F: []
+};
