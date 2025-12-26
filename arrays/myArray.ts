d4598cbf6f3b391f04b0bdb8e4772f2a1a8a11f2
Pilks-pixel <peterpilkington@hotmail.com>
2025-12-26 17:03:10 +0100

    Added shift & unshift  methods

--- arrays/myArray.ts
+++ arrays/myArray.ts
@@ -24,29 +24,49 @@ class MyArray {
 		return lastItem;
 	}
 
+	unshift() {
+		const firstItem = this.data[0];
+		delete this.data[0];
+		this.shiftItems(0);
+		return firstItem;
+	}
+
+	shift(item: any) {
+        this.unshiftItems();
+		this.data[0] = item;
+		this.length++;
+		return this.length;
+	}
+
 	delete(index: number) {
 		const item = this.data[index];
 		this.shiftItems(index);
-        return item;
-    }
+		return item;
+	}
 
-    shiftItems(index : number) {
-        for (let i = index; i < this.length - 1; i++) {
-            this.data[i] = this.data[i + 1];
-            
-        }
-        delete this.data[this.length - 1];
+	shiftItems(index: number) {
+		for (let i = index; i < this.length - 1; i++) {
+			this.data[i] = this.data[i + 1];
+		}
+		delete this.data[this.length - 1];
 		this.length--;
-    }
+	}
 
-    }
+	unshiftItems() {
+		for (let i = this.length; i > 0; i--) {
+			this.data[i] = this.data[i - 1];
+		}
+	}
+}
 
 const myArray = new MyArray();
 myArray.push("Hello");
 myArray.push("World");
 myArray.push("!");
 // myArray.pop();
-console.log(myArray.delete(0));
+// console.log(myArray.delete(0));
+// myArray.shift("First")
+myArray.unshift();
 console.log(myArray.get(0)); // Output: Hello
 console.log(myArray.get(1)); // Output: World
 console.log(myArray);
